74cc6e473457fe8530a29c62aba23c3dd6d77d38
AlainJansen <thealijdis@gmail.com>
2016-12-12 11:45:34 +0100

    Revert "Added method printHello"
    This reverts commit 27e7088b9f779def0beb062eaab8c54eaec39ac7.

--- Hello/src/Hallo/Hello.java
+++ Hello/src/Hallo/Hello.java
@@ -2,12 +2,8 @@ package Hallo;
 
 public class Hello {
 
-	public void printHello() {
-		System.out.println("Hello, World!!");
-	}
 	public static void main(String[] args) {
-		Hello hallo = new Hello();
-		hallo.printHello();
+		System.out.println("Hello, World!!");
 	}
 	
 }

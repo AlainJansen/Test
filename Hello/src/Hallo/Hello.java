224efd24fd3debf5c30e7d4b2eefac286bc02799
AlainJansen <thealijdis@gmail.com>
2016-12-12 11:34:48 +0100

    Hello extra method

--- Hello/src/Hallo/Hello.java
+++ Hello/src/Hallo/Hello.java
@@ -2,8 +2,12 @@ package Hallo;
 
 public class Hello {
 
-	public static void main(String[] args) {
+	public void printHello() {
 		System.out.println("Hello, World!!");
+	}
+	public static void main(String[] args) {
+		Hello hallo = new Hello();
+		hallo.printHello();
 	}
 	
 }
